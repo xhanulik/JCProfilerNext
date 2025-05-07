61bd173449024467da9780eb0ead455fd8e3cd60
Veronika Hanulíková <xhanulik@fi.muni.cz>
2025-05-07 12:40:52 +0200

    Adjustments for JCFROST profiling

--- src/main/java/jcprofiler/profiling/SpaTimeProfiler.java
+++ src/main/java/jcprofiler/profiling/SpaTimeProfiler.java
@@ -1,4 +1,5 @@
 // SPDX-FileCopyrightText: 2022 Lukáš Zaoral <x456487@fi.muni.cz>
+// SPDX-FileCopyrightText: 2023 Antonín Dufka <xhanulik@gmail.com>
 // SPDX-FileCopyrightText: 2025 Veronika Hanulíková <xhanulik@gmail.com>
 // SPDX-License-Identifier: GPL-3.0-only
 
@@ -15,6 +16,11 @@ import jcprofiler.profiling.similaritysearch.models.Trace;
 import jcprofiler.profiling.similaritysearch.Similarity;
 import jcprofiler.util.JCProfilerUtil;
 import org.apache.commons.csv.CSVPrinter;
+import org.bouncycastle.jce.ECNamedCurveTable;
+import org.bouncycastle.jce.interfaces.ECPublicKey;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.bouncycastle.jce.spec.ECNamedCurveParameterSpec;
+import org.bouncycastle.jce.spec.ECParameterSpec;
 import org.bouncycastle.util.encoders.Hex;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -26,6 +32,9 @@ import javax.smartcardio.ResponseAPDU;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.Security;
 import java.util.*;
 
 /**
@@ -39,6 +48,11 @@ public class SpaTimeProfiler extends AbstractProfiler {
     private static Trace delimiterTrace = null;
     Path subtracesDirectory = null;
     int delimiterNum = trapNameMap.size();
+
+    public int CARD = 1;
+    private static int STAGE = 2;
+    private static int parties = 2;
+    private static int threshold = 2;
 
     private static final Logger log = LoggerFactory.getLogger(SpaTimeProfiler.class);
 
@@ -55,6 +69,67 @@ public class SpaTimeProfiler extends AbstractProfiler {
     public SpaTimeProfiler(final Args args, TargetController targetController, final CtModel model) {
         super(args, null, targetController, JCProfilerUtil.getProfiledMethod(model, args.executable), null);
     }
+
+
+    /**
+     * @author: Antonín Dufka
+     */
+    public byte[] recodePoint(byte[] point) {
+        Security.addProvider(new BouncyCastleProvider());
+        ECNamedCurveParameterSpec spec = ECNamedCurveTable.getParameterSpec("secp256k1");
+        return spec.getCurve().decodePoint(point).getEncoded(false); // TODO change if should use compressed points
+    }
+
+    /**
+     * @author: Antonín Dufka
+     */
+    public static byte[] randECPoint() throws Exception {
+        Security.addProvider(new BouncyCastleProvider());
+
+        ECParameterSpec ecSpec_named = ECNamedCurveTable.getParameterSpec("secp256k1");
+        KeyPairGenerator kpg = KeyPairGenerator.getInstance("ECDSA", "BC");
+        kpg.initialize(ecSpec_named);
+        KeyPair pair = kpg.generateKeyPair();
+        ECPublicKey pub = (ECPublicKey) pair.getPublic();
+        return pub.getQ().getEncoded(true);
+    }
+
+    /**
+     * @author: Antonín Dufka
+     */
+    public static byte[] randSecret() {
+        Random rng = new Random();
+        byte[] buffer = new byte[32];
+        rng.nextBytes(buffer);
+        return buffer;
+    }
+
+    /**
+     * @author: Antonín Dufka
+     */
+    public static int[] randParticipants(int include, int size, int max) {
+        ArrayList<Integer> all = new ArrayList<Integer>();
+        for(int i = 0; i < max; ++i) {
+            if (i + 1 == include) {
+                continue;
+            }
+            all.add(i + 1);
+        }
+        Collections.shuffle(all);
+        ArrayList<Integer> result = new ArrayList<Integer>();
+        result.add(include);
+        for(int i = 1; i < size; ++i) {
+            result.add(all.get(i - 1));
+        }
+        Collections.sort(result);
+
+        int[] output = new int[size];
+        for(int i = 0; i < size; ++i) {
+            output[i] = result.get(i);
+        }
+        return output;
+    }
+
 
     /**
      * Run oscillocope.
@@ -90,6 +165,40 @@ public class SpaTimeProfiler extends AbstractProfiler {
                 targetController.resetTriggerStrategy();
                 sendAuxiliaryInputs(round);
 
+                CommandAPDU initAPDU = new CommandAPDU(0, 0, 0, 0);
+                log.info("APDU:{} ", Hex.encode(initAPDU.getBytes()));
+                ResponseAPDU response = targetController.sendAPDU(initAPDU);
+                log.info("RESP: {}", Hex.toHexString(response.getData()));
+
+                // choose arbitrary index
+                CARD = 1;
+                System.out.printf("Card index %d\n", CARD);
+
+                // setup random secret either from arguments or generate random when not supplied
+                byte[] secret = randSecret();
+                byte[] randPoint = randECPoint();
+
+                System.out.printf("Secret share for card %d: %s\n", CARD, new String(Hex.encode(secret)));
+                System.out.printf("Public point %s\n", new String(Hex.encode(randPoint)));
+                byte[] point = recodePoint(randPoint);
+                System.out.printf("(Public group key encoded %s\n", new String(Hex.encode(point)));
+
+                // Send secret and point to the card
+                CommandAPDU setupAPDU = new CommandAPDU(0, 1, threshold, parties, Util.concat(new byte[]{(byte) CARD}, secret, point));
+                log.info("APDU:{} ", Hex.encode(setupAPDU.getBytes()));
+                response = targetController.sendAPDU(setupAPDU);
+                log.info("RESP: {}", Hex.toHexString(response.getData()));
+
+                switch (STAGE) {
+                    case 1:
+                        // profiling COMMIT: INS = 0x02
+                        break;
+                    case 2:
+                        prepareSign();
+                        // profiling COMMIT: INS = 0x07
+                        break;
+                }
+
                 // get APDU which will be measured
                 final CommandAPDU triggerAPDU = getInputAPDU(round);
                 final String input = Util.bytesToHex(triggerAPDU.getBytes());
@@ -107,6 +216,11 @@ public class SpaTimeProfiler extends AbstractProfiler {
                     log.info("Measurements not saved");
                     unsuccessfulMeasurements++;
                 }
+
+                CommandAPDU resetAPDU = new CommandAPDU(0, 5, 0, 0);
+                log.info("APDU:{} ", Hex.encode(resetAPDU.getBytes()));
+                response = targetController.sendAPDU(resetAPDU);
+                log.info("RESP: {}", Hex.toHexString(response.getData()));
             }
             // close connection to oscilloscope
             oscilloscope.finish();
@@ -117,10 +231,55 @@ public class SpaTimeProfiler extends AbstractProfiler {
             if (target != null)
                 targetController.close();
             throw new RuntimeException(e);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
 
         log.info("Final number of measurements: {}", args.repeatCount - unsuccessfulMeasurements);
         log.info("Collecting measurements complete.");
+    }
+
+    private void prepareSign() throws Exception {
+        // 1. Commit
+        byte[] cardData;
+        // randomness generation in here in JCProfilerNext and print out - only when DEBUG = true in JCFROST.java
+        // when no DEBUG, nonces are generated on the card and cannot be printed out
+        byte[] hidingNonceRandomness = randSecret();
+        byte[] bindingNonceRandomness = randSecret();
+        assert(hidingNonceRandomness.length == 32 && bindingNonceRandomness.length == 32);
+        System.out.printf("Card %d hiding randomness %s\n", CARD, new String(Hex.encode(hidingNonceRandomness)));
+        System.out.printf("Card %d binding randomness %s\n", CARD, new String(Hex.encode(bindingNonceRandomness)));
+        CommandAPDU rndSetAPDU = new CommandAPDU(0, 2, 64, 0, Util.concat(hidingNonceRandomness, bindingNonceRandomness));
+        System.out.println(new String(Hex.encode(rndSetAPDU.getBytes())));
+        ResponseAPDU response = targetController.sendAPDU(rndSetAPDU);
+        if (response.getSW() != 0x9000) {
+            System.out.printf("Card %d commit APDU failed %s\n", CARD, Util.bytesToHex(response.getBytes()));
+        }
+        cardData = response.getData();
+        System.out.printf("Card %d commitments %s\n", CARD, new String(Hex.encode(cardData)));
+
+        // 2. Commitments
+        int[] participants = randParticipants(CARD, threshold, parties);
+        for (int identifier : participants) {
+            // setup commitments of this card
+            byte[] hiding = Arrays.copyOfRange(cardData, 0, 33);
+            byte[] binding = Arrays.copyOfRange(cardData, 33, 66);
+            if (identifier != CARD) { // setup commitments send from other parties
+                hiding = randECPoint();
+                binding = randECPoint();
+            }
+            // print commitment in hex
+            System.out.printf("Card %d hiding commitment (public) %s\n", identifier, new String(Hex.encode(hiding)));
+            System.out.printf("Card %d binding commitment (public) %s\n", identifier, new String(Hex.encode(binding)));
+            System.out.printf("Card %d sends public commitments to %d: %s\n", identifier, CARD,
+                    new String(Hex.encode(Util.concat(recodePoint(hiding), recodePoint(binding)))));
+            CommandAPDU commitmentAPDU = new CommandAPDU(0, 3, identifier, 0, Util.concat(recodePoint(hiding), recodePoint(binding)));
+            System.out.println(new String(Hex.encode(commitmentAPDU.getBytes())));
+            response = targetController.sendAPDU (commitmentAPDU);
+            if (response.getSW() != 0x9000) {
+                System.out.printf("Card %d commitment APDU failed %s\n", CARD, Util.bytesToHex(response.getBytes()));
+            }
+        }
     }
 
     /**
@@ -131,14 +290,17 @@ public class SpaTimeProfiler extends AbstractProfiler {
      * @throws RuntimeException if setting the next fatal performance trap failed
      */
     private Trace profileSingleStep(CommandAPDU triggerAPDU) throws CardException {
-        // send one more APDU with message for JCFROST profiling version
-        CommandAPDU preSign = new CommandAPDU(triggerAPDU.getCLA(), (byte) 0x04, triggerAPDU.getP1(), triggerAPDU.getP2(), triggerAPDU.getBytes());
-        log.info("APDU: {}", preSign);
-        ResponseAPDU preResponse = targetController.sendAPDU(preSign);
-        int SW = preResponse.getSW();
-        log.info("RESP: SW={}", String.format("%02X", preResponse.getSW1()) + String.format("%02X", preResponse.getSW2()));
-        if (SW != JCProfilerUtil.SW_NO_ERROR) {
-            throw new RuntimeException("Unexpected SW received when profiling: " + SW);
+        int SW;
+        if (STAGE == 2) {
+            // send one more APDU with message for JCFROST profiling version
+            CommandAPDU preSign = new CommandAPDU(triggerAPDU.getCLA(), (byte) 0x04, triggerAPDU.getP1(), triggerAPDU.getP2(), triggerAPDU.getBytes());
+            log.info("APDU: {}", preSign);
+            ResponseAPDU preResponse = targetController.sendAPDU(preSign);
+            SW = preResponse.getSW();
+            log.info("RESP: SW={}", String.format("%02X", preResponse.getSW1()) + String.format("%02X", preResponse.getSW2()));
+            if (SW != JCProfilerUtil.SW_NO_ERROR) {
+                throw new RuntimeException("Unexpected SW received when profiling: " + SW);
+            }
         }
 
         // set pres-send APDU trigger strategy
