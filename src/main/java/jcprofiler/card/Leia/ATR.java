cf5fc2231320a8d133d5ce210e98bbe1789eea45
Veronika Hanulíková <xhanulik@fi.muni.cz>
2025-05-17 15:29:13 +0200

    Final version of Leia driver support

--- src/main/java/jcprofiler/card/Leia/ATR.java
+++ src/main/java/jcprofiler/card/Leia/ATR.java
@@ -1,6 +1,25 @@
+// SPDX-FileCopyrightText: 2019 The LEIA Team <leia@ssi.gouv.fr>
+// SPDX-FileCopyrightText: 2025 Veronika Hanulíková <xhanulik@gmail.com>
+// SPDX-License-Identifier: BSD-3-Clause
+
 /**
- * Code Copyright (c) 2024, Veronika Hanulikova <xhanulik@gmail.com>
- * Python driver for the LEIA Smart Reader (https://github.com/cw-leia/smartleia) Copyright (c) 2019, The LEIA Team <leia@ssi.gouv.fr>
+ * This file is a derivative work based on code from the SmartLEIA project,
+ * originally developed by the LEIA Team (https://github.com/cw-leia/smartleia),
+ * and licensed under the BSD 3-Clause License.
+ *
+ * Original code licensed under the BSD 3-Clause License:
+ * Copyright (c) 2019, The LEIA Team <leia@ssi.gouv.fr>
+ *
+ * Modifications and translation:
+ * Copyright (c) 2025 Veronika Hanulíková
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted under the terms of the BSD 3-Clause License.
+ * See LICENSES/BSD-3-Clause.txt and THIRD_PARTY_NOTICES.txt for details.
+ *
+ * This file is distributed as part of a larger project (JCProfilerNext),
+ * which is licensed under the GNU General Public License v3.0.
+ * See LICENSE.txt for full licensing information.
  */
 
 package jcprofiler.card.Leia;
